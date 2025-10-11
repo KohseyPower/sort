f63f7a83a7cc80a210bfc97b9a2541821ffa713d
Kohsey DUFOUR <kohsey.dufour@gmail.com>
2025-10-11 20:22:06 +0200

    feat: merge sort
    * Indices during merge not properly shown

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -5,3 +5,4 @@ export { insertionSortGenerator } from "./InsertionSort";
 export { selectionSort } from "./SelectionSort";
 export { selectionSortGenerator } from "./SelectionSort";
 export { randomSortGenerator } from "./RandomSort";
+export { MergeSortGenerator } from "./MergeSort";
